69af5fa28901f05cbdaed99a071e7bc7e80b7941
Romain Bouyé <romain.bouye@gmail.com>
2013-01-16 19:58:21 +0100

    fix link issue on h2 tag

--- public/script_client.js
+++ public/script_client.js
@@ -2,30 +2,30 @@
 	
     var IsHeAtHome = function (name){
         var personDiv = $('#' + name);
-        personDiv.children().last().remove();               
+        var h2Element = personDiv.children().last();
+        h2Element.hide();               
         var loader = $(document.createElement('img'));
         loader.addClass("child").attr("id","loader").attr("src","ajax-loader.gif");
         personDiv.append(loader);
-        $.post('/api/' + name, function(data){               
-            var h2Element = document.createElement('h2');
+        $.post('/api/' + name, function(data){
             if(data === "time out"){
-                $(h2Element).html("Time Out ... Sorry!");
+                $(h2Element).html("Timeout... Sorry!");
             }else{                 
-                $(h2Element).html(data).addClass(data);
+                $(h2Element).html(data).attr('class', data + " child");
             }
-            personDiv.children().last().remove();   
-            personDiv.append(h2Element);
+            loader.remove();   
+            h2Element.show();
         });
     }
     
     $("#container").click(function(ev){
         var target = ev.target;
         var className = $(target).attr('class');
-        if (className == "child"){
+        if ($(target).hasClass("child")){
             target = $(target).parent()[0];
             className = $(target).attr('class');
         }
-        if("person" === $(target).attr('class')){
+        if("person" === className){
             IsHeAtHome($(target).attr("id"))
         }
     });
