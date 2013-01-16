180f5675376beae5d087413754490b3c2e17f17d
Eric Allard <eric.allard0104@gmail.com>
2013-01-16 15:57:13 +0100

    Adding feature to choose the person to ping

--- public/script_client.js
+++ public/script_client.js
@@ -1,20 +1,32 @@
 (function(){
 	
-	    var whoIsAtHome = function (){ 
-			$.post('/api', function(data){
-				if(data === "time out"){
-					$('#container').html("<p>Time Out ... Sorry!</p>");
-				}else{
-					$('.loader').remove();
-					data = JSON.parse(data);
-					var names = data.name;
-					for(var i=0; i<names.length; i++){               
-					   var h2Element = document.createElement('h2');
-					   $(h2Element).html(data.result[i]);
-					   $('#' + names[i]).append(h2Element);
-					}
-				}
-			});
-		}();
-	
+    var IsHeAtHome = function (name){
+        var personDiv = $('#' + name);
+        personDiv.children().last().remove();               
+        var loader = $(document.createElement('img'));
+        loader.addClass("child").attr("id","loader").attr("src","ajax-loader.gif");
+        personDiv.append(loader);
+        $.post('/api/' + name, function(data){               
+            var h2Element = document.createElement('h2');
+            if(data === "time out"){
+                $(h2Element).html("Time Out ... Sorry!");
+            }else{                 
+                $(h2Element).html(data);
+            }
+            personDiv.children().last().remove();   
+            personDiv.append(h2Element);
+        });
+    }
+    
+    $("#container").click(function(ev){
+        var target = ev.target;
+        var className = $(target).attr('class');
+        if (className == "child"){
+            target = $(target).parent()[0];
+            className = $(target).attr('class');
+        }
+        if("person" === $(target).attr('class')){
+            IsHeAtHome($(target).attr("id"))
+        }
+    });
 })();
--- server.js
+++ server.js
@@ -1,62 +1,67 @@
 var nodeStatic = require('node-static'),
-	fserver = new nodeStatic.Server('./public'),
-	exec = require('child_process').exec;
+fserver = new nodeStatic.Server('./public'),
+exec = require('child_process').exec;
+
+var ips = {
+    ip: ["192.168.1.18", "192.168.1.20"],
+    name:["Romain", "Eric"],
+    email:["romain.bouye@gmail.com","eric.allard0104@gmail.com"]
+};
 
 require('http').createServer(function (request, response) {
-	if(request.url == "/api"){
-		var ips = {
-			ip: ["192.168.1.18", "192.168.1.20"],
-			name:["Romain", "Eric"],
-			result:[]
-		};
-		console.log(request.url);
-
-		request.addListener('end', function () {
-			setTimeout(function(){
-			response.writeHead(200, {'Content-Type': 'text/html'});
-			response.write("time out");
-			response.end();
-		  }, 5500);
-				
-		var message = '';
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		pingNextPerson(ips, 0, response);
-		});
-	}else if(request.url == "/design.css"){
-			fserver.serveFile('/design.css',200,{},request,response);
-	}else if(request.url == "/script_client.js"){
-			fserver.serveFile('/script_client.js',200,{},request,response);
-	}else if(request.url == "/jquery-1.6.4.min.js"){
-			fserver.serveFile('/jquery-1.6.4.min.js',200,{},request,response);
-	}else if(request.url == "/ajax-loader.gif"){
-			fserver.serveFile('/ajax-loader.gif',200,{},request,response);
-	}else{
-			fserver.serveFile('/index.html',200,{},request,response);
-	}
+    var req = request.url.split("/");
+    req.shift();
+    if(req[0] == "api"){
+        var name = req[1];
+        request.addListener('end', function () {
+            setTimeout(function(){
+                response.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                response.write("time out");
+                response.end();
+            }, 5500);
+            response.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            pingAPerson(name,response);
+        });
+    }else if(req[0] == "design.css"){
+        fserver.serveFile('/design.css',200,{},request,response);
+    }else if(req[0] == "script_client.js"){
+        fserver.serveFile('/script_client.js',200,{},request,response);
+    }else if(req[0] == "jquery-1.6.4.min.js"){
+        fserver.serveFile('/jquery-1.6.4.min.js',200,{},request,response);
+    }else if(req[0] == "ajax-loader.gif"){
+        fserver.serveFile('/ajax-loader.gif',200,{},request,response);
+    }else{
+        fserver.serveFile('/index.html',200,{},request,response);
+    }
 
 }).listen(8888);
 
-function pingNextPerson(ips, id, response){
-	if(id <= ips.ip.length-1){
-	exec("ping "+ips.ip[id]+" -c 2 -W 2", function(error, stdout, stderr) {
 
-				var name = ips.name[id];
-				console.log('exec' + name);
-				var the6thElementOf2ndeLineOfTheResultOfPing = stdout.split('\n')[1].split(' ')[5];
-				
-				if(the6thElementOf2ndeLineOfTheResultOfPing == "ttl=64"){
-					message = "Present";
-				} else if (the6thElementOf2ndeLineOfTheResultOfPing == "Unreachable"){
-					message = "Absent";
-				} else {
-				  message = "Perdu";
-				}
-				ips.result[id] = message;
-				pingNextPerson(ips, ++id, response);
-	});
-	} else{
-		response.write(JSON.stringify(ips));
-		response.end();
-		console.log('terminé');
-	}	
+function pingAPerson(name,response){
+    var id = ips.name.indexOf(name);
+    ping(id,response);
+}
+
+function ping(id,response){
+   return exec("ping "+ips.ip[id]+" -c 2 -W 2", function(error, stdout, stderr) {
+        var name = ips.name[id];
+        console.log('exec ' + name);
+        var the6thElementOf2ndeLineOfTheResultOfPing = stdout.split('\n')[1].split(' ')[5];
+        var message;
+                
+        if(the6thElementOf2ndeLineOfTheResultOfPing == "ttl=64"){
+            message =  "Present";
+        } else if (the6thElementOf2ndeLineOfTheResultOfPing == "Unreachable"){
+            message = "Absent";
+        } else {
+            message = "Perdu";
+        }  
+        response.write(message);
+        response.end();
+        console.log('terminé');
+    });
 }
