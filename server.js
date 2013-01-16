d0d024e138f7bca912bbaa811b467343abb8b833
Eric Allard <eric.allard0104@gmail.com>
2013-01-17 00:22:51 +0100

    Persons and IPs are now stored in a JSON file

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ sys = require('sys'),
 fserver = new nodeStatic.Server('./public'),
 exec = require('child_process').exec;
 
-var ips = {
-    ip: ["192.168.1.18", "192.168.1.20"],
-    name:["Romain", "Eric"],
-    email:["romain.bouye@gmail.com","eric.allard0104@gmail.com"]
-};
+var personsFile = fs.readFileSync('./persons.json','utf8'); 
+var ips = JSON.parse(personsFile); 
 
 require('http').createServer(function (request, response) {
     var req = request.url.split("/");
@@ -41,12 +38,29 @@ require('http').createServer(function (request, response) {
         readIndex(function (indexHtml) {
             jsdom.env({
                 html: indexHtml,
-                scripts: ['./public/jquery-1.6.4.min.js', './public/script_client.js']
+                scripts: ['http://code.jquery.com/jquery-1.9.0.min.js', 'https://raw.github.com/placemarker/jQuery-MD5/master/jquery.md5.js','/script_client.js']
             }, function (err, window) {
                 var $ = window.jQuery;
-
-                $('body').append("<div class='testing'>Hello World</div>");
-                console.log($(".testing").html()); // outputs Hello World
+                for(var id = 0; id< ips.ip.length ;id++){
+                    var $divPersonElement = $('<div>');
+                    console.log('test du for');
+                    $divPersonElement.attr('id', ips.name[id]).addClass("person");
+                    var $img = $('<img>');
+                    $img.attr('src', 'http://www.gravatar.com/avatar/'+ $.md5(ips.email[id])+'?s=400').addClass('child');
+                    var $h2PersonName = $('<h2>');
+                    $h2PersonName.html(ips.name[id]).addClass('child');
+                    $divPersonElement.append($img).append($h2PersonName);
+                    console.log($divPersonElement.html());
+                    $('#container').append($divPersonElement);
+                }
+                
+                $('.jsdom').removeClass('jsdom');
+                  response.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                response.write(window.document.innerHTML);
+                response.end();
+                
             });
         })
         
